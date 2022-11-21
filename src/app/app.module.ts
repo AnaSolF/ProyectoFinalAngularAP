c710001eec698f46112c1a1921612cb35c70cb63
Ana Sol Franchisena <anasolfran@hotmail.com>
2022-11-21 16:08:58 -0300

    Imports
    Agregar import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { HardSoftComponent } from './hard-soft/hard-soft.component'; 
+import { EducacionComponent } from './educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    SobreMiComponent,
+    ProyectosComponent,
+    HardSoftComponent,
+    EducacionComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule
