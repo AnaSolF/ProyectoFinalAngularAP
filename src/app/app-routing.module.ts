34918669a57986c3ad06e488297473b8e4bbdac5
Ana Sol Franchisena <anasolfran@hotmail.com>
2022-11-20 21:29:52 -0300

    Nuevos componentes y rutas.
    Creación de componentes y modificación del archivo app.routing.module.ts .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Importamos las rutas 
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component'
+import { ProyectosComponent } from './proyectos/proyectos.component'
+import { HardSoftComponent } from './hard-soft/hard-soft.component'
+import { EducacionComponent } from './educacion/educacion.component'
+import { ContactoComponent } from './contacto/contacto.component'
+
+const routes: Routes = [
+  {path: 'sobre-mi', component: SobreMiComponent},
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'hard-soft', component: HardSoftComponent },
+  { path: 'educacion', component: EducacionComponent },
+  {path: 'contacto', component: ContactoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
